b64091f20f94f877115ab150ce6a9187d5d3c64e
Brad McNaughton <bradamcnaughton@gmail.com>
2024-12-17 14:21:29 +1030

    Clean up SSL testing attempts

--- src/main/java/ca/uhn/fhir/jpa/starter/common/FhirClientConfig.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/common/FhirClientConfig.java
@@ -5,10 +5,8 @@ import javax.net.ssl.SSLContext;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
@@ -16,15 +14,6 @@ import ca.uhn.fhir.rest.client.api.ServerValidationModeEnum;
 
 @Configuration
 public class FhirClientConfig {
-
-    @Value("${server.ssl.trust-store}")
-    private Resource trustStore;
-
-    @Value("${server.ssl.trust-store-password}")
-    private String trustStorePassword;
-
-    @Value("${server.ssl.trust-store-type}")
-    private String trustStoreType;
 
     @Autowired
     private SSLContext sslContext;
